207a41d6e95fdcf814bf3d93993a6f6e2e8da0b8
desoyeDami <desoyedami@gmail.com>
2024-08-03 14:50:30 +0100

    Prerequisites

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" """
+
 from flask import Blueprint
 
 app_views = Blueprint('api_views', __name__, url_prefix='/api/v1')
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" """
+
 from api.v1.views import app_views
 from flask import Flask, jsonify
 
