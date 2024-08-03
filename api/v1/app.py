861c5f9c7b12cf488e079d173db23f453f4369f4
desoyeDami <desoyedami@gmail.com>
2024-08-03 14:51:39 +0100

    Prerequisites

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" """
+
 from api.v1.views import app_views
 from flask import Flask
 from models import storage
@@ -11,6 +13,7 @@ app.register_blueprint(app_views)
 
 @app.teardown_appcontext
 def teardown(exception):
+    """ """
     storage.close()
 
 
